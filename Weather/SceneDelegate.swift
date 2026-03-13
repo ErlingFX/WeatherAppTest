48c0f598162b5ad1139bf8d64f857c6efaebe16f
Александр Назаров <erlingfx@gmail.com>
2026-03-13 15:33:20 +0300

    stage 1

---
+++ Weather/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  Weather
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        let weatherVC = WeatherRouter.createModule()
+        window?.rootViewController = weatherVC
+        window?.makeKeyAndVisible()
+    }
+}
